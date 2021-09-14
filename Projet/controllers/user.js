0ad1f2f383f06f5337272b1bdd743130507643f1
Manon Dufour <manon.dufour95@gmail.com>
2021-09-14 15:46:35 +0200

    Finalisation du projet

---
+++ Projet/controllers/user.js
@@ -0,0 +1,53 @@
+const bcrypt = require('bcrypt');
+const jwt = require('jsonwebtoken');
+const passwordValidator = require('password-validator');
+const User = require('../models/User');
+
+const schema = new passwordValidator();
+
+schema.is().min(5).has().uppercase().has().digits(1).has().not().spaces();
+
+
+
+exports.signup = (req, res, next) => {
+  if(schema.validate(req.body.password)) {
+    bcrypt.hash(req.body.password, 10)
+    .then(hash => {
+      const user = new User({
+        email: req.body.email,
+        password: hash
+      });
+      user.save()
+      .then(() => res.status(201).json({ message: 'Utilisateur créé !' }))
+      .catch(error => res.status(400).json({ error }));
+    })
+    .catch(error => res.status(500).json({ error }))
+  } else if(!schema.validate(req.body.password)) {
+    return res.status(400).json({ error });
+  }
+};
+
+  exports.login = (req, res, next) => {
+    User.findOne({ email: req.body.email })
+      .then(user => {
+        if (!user) {
+          return res.status(401).json({ error });
+        }
+        bcrypt.compare(req.body.password, user.password)
+          .then(valid => {
+            if (!valid) {
+              return res.status(401).json({ error });
+            }
+            res.status(200).json({
+              userId: user._id,
+              token: jwt.sign(
+                { userId: user._id },
+                'RANDOM_TOKEN_SECRET',
+                { expiresIn: '24h' }
+              )
+            });
+          })
+          .catch(error => res.status(500).json({ error }));
+      })
+      .catch(error => res.status(500).json({ error }));
+  };
